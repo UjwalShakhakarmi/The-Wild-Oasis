3096d92a2b8dc07f9486759d5aee6ad4fe1b832a
UjwalShakhakarmi <ujwalshakha@gmail.com>
2024-05-09 00:11:06 +0545

    inital

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ggllrdzjdvlseiqpliwp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnbGxyZHpqZHZsc2VpcXBsaXdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxNTgxNzksImV4cCI6MjAzMDczNDE3OX0.b3rU1-Mfb_hRMxhs9FAxgZe_KtSPDZOG6M9TWT0zfDg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
