3bd166831df03b65fe5d1211363337bafe0a0dde
UjwalShakhakarmi <ujwalshakha@gmail.com>
2024-06-11 12:36:32 +0545

    implemented pagination

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -2,7 +2,6 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import BookingTable from "../features/bookings/BookingTable";
 import BookingTableOperations from "../features/bookings/BookingTableOperations";
-import { useSearchParams } from "react-router-dom";
 
 function Bookings() {
   return (
